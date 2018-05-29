8c31373cae2bcb879a5cc127646b26b94ed924a3
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-29 15:23:14 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@
 def turn_count(board)
   gameCount = 0
   board.each do |cube|
-    if board.length < 9 && cube == "X" || "O"
+    if cube == "X" || "O"
       gameCount += 1 
     else 
       return gameCount
