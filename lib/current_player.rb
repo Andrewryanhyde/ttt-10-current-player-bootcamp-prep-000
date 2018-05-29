50b597901a49c0870b59f0948ac900d1ef65c587
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-29 15:53:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,8 +12,8 @@ end
   def current_player(board)
     gameCount = turn_count(board)
     if gameCount % 2 == 0 
-      return "O"
-    else 
       return "X"
+    else 
+      return "O"
     end
   end
